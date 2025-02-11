56957bf91529e2cffb4434ae16a7713513debd33
TheDogHusky <contact@classydev.fr>
2025-02-11 18:44:23 +0100

    chore: port for production

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { toHTML } = require('@odiffey/discord-markdown');
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use("/static", express.static("static"));
